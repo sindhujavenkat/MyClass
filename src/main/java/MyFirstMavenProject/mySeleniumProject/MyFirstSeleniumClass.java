0688ed9c1036abe18402a5a6f195429b4399a39a
Sindhuja <sindhujavenkat@gmail.com>
2019-02-14 09:07:25 +0530

    first commit

---
+++ src/main/java/MyFirstMavenProject/mySeleniumProject/MyFirstSeleniumClass.java
@@ -0,0 +1,19 @@
+package MyFirstMavenProject.mySeleniumProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstSeleniumClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.in");
+		driver.quit();
+		
+	}
+
+}
